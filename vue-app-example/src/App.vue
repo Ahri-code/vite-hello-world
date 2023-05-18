8419b2c4d9012498ee1997a4db04cf82a5fda387
Edy Galdiolo <edygaldy302@gmail.com>
2023-05-18 16:58:49 +0200

    first commit

---
+++ vue-app-example/src/App.vue
@@ -0,0 +1,36 @@
+
+<script>
+//Importiamo i componenti da usare
+import AppTitle from './components/AppTitle.vue'
+import AppContent from './components/AppContent.vue'
+import AppButton from './components/AppButton.vue'
+
+//Definiamo l'oggetto che conterrà la lista dei componenti importati, che vogliamo usare
+export default {
+  components: {
+    AppTitle,
+    AppContent,
+    AppButton
+  }
+}
+</script>
+
+<!-- Qui inseriamo l'HTML della nostra App -->
+<template>
+  <header>
+    <!-- Usiamo i componenti importati sopra -->
+    <!-- il contenuto è nei relativi file (cartella "components") -->
+    <AppTitle />
+  </header>
+
+  <main>
+    <!-- Usiamo i componenti importati sopra -->
+    <!-- il contenuto è nei relativi file (cartella "components") -->
+    <AppContent />
+
+    <AppButton />
+  </main>
+</template>
+
+<!-- Qui abbiamo eventuale stile del componente "App", che è il contenitore generale -->
+<style></style>
---
+++ vue-app-example/src/components/AppButton.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+    name: "AppButton"
+}
+</script>
+
+<template>
+    <button>
+        Un bottone
+    </button>
+</template>
+
+<style>
+button {
+    border: none;
+    border-radius: 10%;
+    background-color: #27bc6f;
+    padding: 1rem;
+    font-size: 20px;
+    margin-top: 1rem;
+}
+</style>
