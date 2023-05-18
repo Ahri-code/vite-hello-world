ec20e2be551bac33e39d07959a8dc52347a20b43
Edy Galdiolo <edygaldy302@gmail.com>
2023-05-18 17:17:40 +0200

    feat(vue): apptitle

---
+++ hello-world/src/components/AppTitle.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+    name: "AppTitle"
+}
+</script>
+
+<template>
+    <div>
+        <h1>La mia prima app con Vite!</h1>
+    </div>
+</template>
+
+<style>
+h1 {
+    color: white;
+}
+</style>
